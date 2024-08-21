772b2ad7cf68c383d1726904f8a55806df8e28c8
Polygonal Graphics <154336865+Polygonal-Graphics@users.noreply.github.com>
2024-08-21 10:17:34 -0500

    Added some comments to the TestApp

--- Test-App/Source/main.cpp
+++ Test-App/Source/main.cpp
@@ -7,12 +7,19 @@ using namespace Polygame;
 
 int main()
 {
+	// Init Game (and GLAD)
 	Game::Init();
+
+	// Create the scene and GameObjects
 	Scene* scene = new Scene;
 	Background* background = new Background;
 	Player* player = new Player;
+
+	// Add the GameObjects to the scene
 	scene->AddGameObject(background);
 	scene->AddGameObject(player);
+
+	// Set the game scene and start the game
 	Game::SetScene(scene);
 	Game::Start();
 
