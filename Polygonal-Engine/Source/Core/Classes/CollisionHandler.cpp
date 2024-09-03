d2f1223fb1876404fccb00ff9dae8eebd3dd6067
Polygonal Graphics <154336865+Polygonal-Graphics@users.noreply.github.com>
2024-09-03 12:37:29 -0500

    Create CollisionHandler.cpp

---
+++ Polygonal-Engine/Source/Core/Classes/CollisionHandler.cpp
@@ -0,0 +1,14 @@
+#include "CollisionHandler.h"
+
+namespace Polygame
+{
+  void CollisionHandler::CheckCollisions(GameObject* sceneRoot)
+  {
+    
+  }
+
+  bool CollisionHandler::BoundingBoxCollisionCheck(ColliderComponent* colliderA, ColliderComponent* colliderB)
+  {
+    
+  }
+{
