e3c60c6020a752ea3d642b616ff9ab9c720eea38
Polygonal Graphics <154336865+Polygonal-Graphics@users.noreply.github.com>
2024-09-03 12:33:09 -0500

    Update CollisionHandler interface

--- Polygonal-Engine/Source/Core/Classes/CollisionHandler.h
+++ Polygonal-Engine/Source/Core/Classes/CollisionHandler.h
@@ -1,7 +1,18 @@
-
 namespace Polygame
-class CollisionHandler
 {
-  public:
-    void CheckCollisions()
+  class GameObject;
+  class ColliderComponent;
+
+  // A class that takes in a scene root and performs collision checks on the colliders attached to GameObjects in that scene.
+  class CollisionHandler
+  {
+    public:
+    // Takes in a scene root and collects all collisions. Then, checks if dynamic colliders have collided with any
+    // other dynamic colliders or static colliders, and calls their OnCollision methods.
+    void CheckCollisions(GameObject* sceneRoot);
+
+    private:
+    // Checks if the given bounding boxes are colliding / overlapping.
+    bool BoundingBoxCollisionCheck(ColliderComponent* colliderA, ColliderComponent* colliderB);
+  }
 }
