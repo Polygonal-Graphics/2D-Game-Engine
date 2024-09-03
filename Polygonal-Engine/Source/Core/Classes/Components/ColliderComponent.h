244669b263e75b37c42efd073b5bf822d46bb812
Polygonal Graphics <154336865+Polygonal-Graphics@users.noreply.github.com>
2024-09-03 13:25:14 -0500

    Update ColliderComponent.h

--- Polygonal-Engine/Source/Core/Classes/Components/ColliderComponent.h
+++ Polygonal-Engine/Source/Core/Classes/Components/ColliderComponent.h
@@ -4,13 +4,13 @@
 
 namespace Polygame
 {
-  class ColliderComponent : BaseComponent
-  {
+    class ColliderComponent : BaseComponent
+    {
     public:
 
     private:
-      // True if a collision was detected with this collider during the frame
-      bool m_IsColliding = false;
-      
-  }
+        // True if a collision was detected with this collider during the frame
+        bool m_IsColliding = false;
+        
+    }
 }
